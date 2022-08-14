5a5ab072d9c546d653ea5ed311fc3f7f0aaae302
Anthea Chen <anxuec2@illinois.edu>
2022-08-14 14:14:52 -0500

    add zsh installation

--- setup.sh
+++ setup.sh
@@ -5,5 +5,9 @@ cp vimrc ~/.vimrc && \
     cp keybindings.tmux ~/.byobu/keybindings.tmux && \
     cp tmux.conf ~/.byobu/.tmux.conf && \
     tmux source-file ~/.byobu/.tmux.conf && \
+	echo "zsh" >> ~/.bashrc && \
+	source ~/.bashrc && \
+    wget -O ohmyzsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh && \
+    sh ~/ohmyzsh.sh && \
     byobu kill-server && \
-    byobu
+	byobu
